2fbda008e6c20eeeb584efe6295a0c07181eac2e
marciock <marciockalves@gmail.com>
2017-09-06 19:33:57 -0300

    nova pasta de atividades

---
+++ menulist/js/db.js
@@ -0,0 +1,30 @@
+const banco=[
+  {
+    'nome':'Marcio',
+    'telefone':'33673443',
+    'celuar':'99955455',
+    'email':'marcio@trosoba.com',
+    'cidade':'Umuarama'
+  },
+  {
+    'nome':'José',
+    'telefone':'3364888',
+    'celuar':'9978888',
+    'email':'jose@trosoba.com',
+    'cidade':'Cruzeiro do Oeste',
+  },
+  {
+    'nome':'Bruno',
+    'telefone':'3367555',
+    'celuar':'99888888',
+    'email':'bruno@trosoba.com',
+    'cidade':'Tapira'
+  },
+  {
+    'nome':'Genésio',
+    'telefone':'3369999',
+    'celuar':'9988777',
+    'email':'genesio@trosoba.com',
+    'cidade':'Carboneira'
+  }
+]
